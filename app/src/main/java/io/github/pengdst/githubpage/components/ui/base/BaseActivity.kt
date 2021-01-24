8a7a90e33f4d486c15d0ad492bdfc296aff85485
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-01-25 00:50:54 +0700

    Delete unused generic type

--- app/src/main/java/io/github/pengdst/githubpage/components/ui/base/BaseActivity.kt
+++ app/src/main/java/io/github/pengdst/githubpage/components/ui/base/BaseActivity.kt
@@ -2,9 +2,8 @@ package io.github.pengdst.githubpage.components.ui.base
 
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
 
-abstract class BaseActivity<VM: ViewModel>: AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity() {
 
     fun shortToast(message: String?){
         Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
