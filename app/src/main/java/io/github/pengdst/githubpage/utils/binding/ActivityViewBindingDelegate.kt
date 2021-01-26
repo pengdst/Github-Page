a4cb8a31a53df58a7507252f5fbadf11d704639f
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-01-26 08:03:37 +0700

    membuat viewBinding delegation class

---
+++ app/src/main/java/io/github/pengdst/githubpage/utils/binding/ActivityViewBindingDelegate.kt
@@ -0,0 +1,57 @@
+package io.github.pengdst.githubpage.utils.binding
+
+import android.app.Activity
+import android.view.LayoutInflater
+import androidx.annotation.MainThread
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+/**
+ *
+ * Created by Pengkuh Dwi Septiandi on 1/26/21.
+ *
+ * Github    : https://github.com/pengdst
+ * Gitlab    : https://gitlab.com/pengdst
+ * LinkedIn  : https://linkedin.com/in/pengkuh-dst/
+ *
+ */
+
+class ActivityViewBindingDelegate<T : ViewBinding>(private val bindingClass: Class<T>) :
+    ReadOnlyProperty<Activity, T> {
+
+
+    companion object {
+        @MainThread
+        inline fun <reified T : ViewBinding> AppCompatActivity.viewBindings(): ActivityViewBindingDelegate<T> =
+            ActivityViewBindingDelegate(T::class.java)
+    }
+
+    /**
+     * initiate variable for binding view
+     */
+    private var binding: T? = null
+
+    @Suppress("UNCHECKED_CAST")
+    override fun getValue(thisRef: Activity, property: KProperty<*>): T {
+        binding?.let { return it }
+
+        /**
+         * inflate View class
+         */
+        val inflateMethod = bindingClass.getMethod("inflate", LayoutInflater::class.java)
+
+        /**
+         * Bind layout
+         */
+        val invokeLayout = inflateMethod.invoke(null, thisRef.layoutInflater) as T
+
+        /**
+         * Set the content view
+         */
+        thisRef.setContentView(invokeLayout.root)
+
+        return invokeLayout.also { this.binding = it }
+    }
+}
